0219ed3700101a129c8f85d26806371680c062b7
Medy <mannomedy@gmail.com>
2025-06-08 22:37:49 +0300

    add changes

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return messages;
+}
+
+// Example usage:
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+// Example usage:
+countDown(10);
